54940ef2a7a95d913481288728ac0181184a6912
nikhilgangeri <nikhilgangeri@gmail.com>
2022-09-20 15:01:52 +0530

    removed cors

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ db.sequelize.sync() //Synced sequelize
 
   
 //Middlewares
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
